facd11b35875ba8760ce5b3d1c42102e885afe3a
webgtx <dxv1d@tuta.io>
2021-03-19 18:51:59 +0200

    Fixed bug with $USER

--- install.sh
+++ install.sh
@@ -10,15 +10,14 @@ echo "		2) Debian & Ubuntu"
   read installOption
 
 if [[ $installOption = "1" ]]; then
-    pacman -S i3-gaps i3-gaps i3blocks i3lock i3status
-    pacman -S termite picom nitrogen
-    toilet --metal Done
     echo ""
     echo "You wanna use my i3wm config or your own?"
     echo "Input (own/your)"
     read configOption
       if [[ $configOption = 'your' ]]; then
         cp .config -r /home/$USER/
+        pacman -S i3-gaps i3-gaps i3blocks i3lock i3status
+        pacman -S termite picom nitrogen
         echo "Done, now you need log out from your DE, and run I3WM"
       fi
     echo ""
