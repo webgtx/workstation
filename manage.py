fcfc7edd024b3fb5adcfcb992c403bbecb96b5af
webgtx <alex@zolotarov.me>
2024-06-02 09:14:16 -0700

    Update: add whitelist & finish import_dotfiles implementation

--- manage.py
+++ manage.py
@@ -1,14 +1,18 @@
+#!/bin/python3
 from prompt_toolkit import prompt, HTML
 from prompt_toolkit import print_formatted_text as print
 from prompt_toolkit.styles import Style
 from prompt_toolkit.shortcuts import message_dialog, input_dialog, radiolist_dialog
 from shutil import copytree, copy
 from pathlib import Path
+import yaml
 
 theme = Style.from_dict({
     "dialog": "bg:#282c34",
 })
 
+with open("whitelist.yaml", "r") as f:
+    whitelist = yaml.safe_load(f)
 
 # https://stackoverflow.com/questions/78430874/copying-files-and-directories-using-a-single-method-from-shutil-copytree-is-not
 def copyall(src: str, dst: str):
@@ -20,28 +24,28 @@ def copyall(src: str, dst: str):
             else:
                 copy(file_path, Path(dst) / file_path.name)
 
+task = radiolist_dialog(
+    title="Workspace Manager",
+    text="Select dotfiles",
+    style=theme,
+    values=[
+        ("import_dotfiles", "Import dotfiles"),
+        ("install_dotfiles", "Install dotfiles"),
+        ("install_rcfiles", "Install rcfiles"),
+    ]
+).run()
 
-while 1:
-    task = radiolist_dialog(
-        title="Workspace Manager",
-        text="Select dotfiles",
-        style=theme,
-        values=[
-            ("import_dotfiles", "Import dotfiles"),
-            ("install_dotfiles", "Install dotfiles"),
-            ("install_rcfiles", "Install rcfiles"),
-        ]
-    ).run()
+match task:
+    case "import_dotfiles":
+        for software in whitelist["dotfiles"]:
+            copyall(Path.home() / ".config" / software, Path("configs") / software)
+            print(f"import {software} to the repository")
 
-    match task:
-        case "import_dotfiles":
-           copyall(Path.home() / ".config", "./configs")
+    case "install_dotfiles":
+        copyall("./configs", Path.home() / ".config")
 
-        case "install_dotfiles":
-           copyall("./configs", Path.home() / ".config")
-
-        case "install_rcfiles":
-           for rcfile in Path("./rcs").iterdir():
-               copy(rcfile, Path.home() / f".{rcfile}")
-        case _:
-            exit(0)
+    case "install_rcfiles":
+        for rcfile in Path("./rcs").iterdir():
+            copy(rcfile, Path.home() / f".{rcfile}")
+    case _:
+        exit(0)
